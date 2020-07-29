8e3f6b6ec6177753999bcc7e8a1c8247e6b7cc18
Matthew <Staalkoper@gmail.com>
2020-07-29 02:12:27 +0200

    More recent confirmation.py

--- utils/confirmation.py
+++ utils/confirmation.py
@@ -1,15 +1,12 @@
+
 from utils.common_embed import *
 import asyncio
-import typing
-import discord
 
 
-# confirmation takes title str, confirmation_msg_input str, command str (name of command) and return_message_object bool default False
+# confirmation takes title str, confirmation_msg_input str and command str (name of command)
 #  asks user for confirmation of action through reactions
-#  returns bool or tuple(bool, discord.Message) based on reaction
-async def confirmation(bot, ctx, title: str, confirmation_msg_input: str, command: str, return_message_object: bool = False) -> \
-                                                                typing.Union[bool, typing.Union[bool, discord.Message]]:
-
+#  returns bool based on reaction
+async def confirmation(bot, ctx, title: str, confirmation_msg_input: str, command: str) -> bool:
     confirmation_msg = await ctx.send(embed=common_embed(title, confirmation_msg_input))
     await confirmation_msg.add_reaction("✅")
     await confirmation_msg.add_reaction("❌")
@@ -26,18 +23,18 @@ async def confirmation(bot, ctx, title: str, confirmation_msg_input: str, comman
             await confirmation_msg.edit(embed=common_embed(title,
                                                            "Shaking my fucking head. Please start over and don't "
                                                            "be a fool."))
-            return (confirmation_msg, False) if return_message_object else False
+            return False
         elif reaction.emoji == "❌":
             await confirmation_msg.edit(embed=common_embed(title,
                                                            "The request is cancelled. "
                                                            f"Type `!help {command}` to start over."))
-            return (confirmation_msg, False) if return_message_object else False
+            return False
         elif reaction.emoji == "✅":
             await confirmation_msg.edit(embed=common_embed(title,
                                                            f"The {title.lower()} request is confirmed."))
-        return (confirmation_msg, True) if return_message_object else True
+        return True
 
     except asyncio.TimeoutError:
         await confirmation_msg.edit(embed=common_embed(title,
                                                        "Looks like you waited too long. Please restart the process."))
-        return (confirmation_msg, False) if return_message_object else False
+        return False
